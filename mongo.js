56866a4516c027e4edc1ee36c8ebd204501e30ff
Rikkokiri <pilvi.rajala@gmail.com>
2019-03-15 17:35:52 +0200

    Database connection tested. Works.

---
+++ mongo.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+const url = process.env.MONGODB_URI
+console.log('URL', url)
+
+mongoose.connect(url, { useNewUrlParser: true })
+
+const Reading = mongoose.model('Reading', {
+  sensorname: String,
+  temperature: Number,
+  pressure: Number,
+  humidity: Number,
+  date: Date
+})
+
+const reading = new Reading({
+  sensorname: 'YokkilaSensor',
+  temperature: 25.58,
+  pressure: 98956.52,
+  humidity: 21.87207,
+  date: new Date()
+})
+
+reading
+  .save()
+  .then(response => {
+    console.log('reading saved!')
+    mongoose.connection.close()
+  })
