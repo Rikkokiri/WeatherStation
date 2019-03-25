9681a75ea60b5ddc3912b2d89c6be2f27ab6be3b
Rikkokiri <pilvi.rajala@gmail.com>
2019-03-25 21:26:51 +0200

    Made the clock tick

--- client/src/components/TimeDateDisplay.js
+++ client/src/components/TimeDateDisplay.js
@@ -1,23 +1,52 @@
 import React from 'react'
-import Typography from '@material-ui/core/Typography'
+import Typography from '@material-ui/core/Typography';
 
-const TimeDateDisplay = () => {
-  const time = new Date()
-  var dateOptions = { weekday: 'long', month: 'long', day: 'numeric' };
+var dateOptions = { weekday: 'long', month: 'long', day: 'numeric' };
 
-  return (
-    <div className="timeDateDisplay">
-      <Typography variant="h2">
-        {time.toLocaleString(undefined, {
-          hour: '2-digit',
-          minute: '2-digit'
-        })}
-      </Typography>
-      <Typography variant="h6">
-        {time.toLocaleDateString('en-GB', dateOptions)}
-      </Typography>
-    </div>
-  )
+class TimeDateDisplay extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      time: new Date()
+    }
+  }
+
+  // When component is inserted into the DOM
+  componentDidMount() {
+    this.intervalID = setInterval(
+      () => this.tick(),
+      1000
+    )
+  }
+
+  // When component is removed from the DOM
+  componentWillUnmount() {
+    clearInterval(this.intervalID)
+  }
+
+  tick() {
+    this.setState({
+      time: new Date()
+    })
+  }
+
+  render() {
+    return (
+      <div className="timeDateDisplay">
+        <Typography variant="h2">
+          {this.state.time.toLocaleString(undefined, {
+            hour: '2-digit',
+            minute: '2-digit',
+            second: '2-digit'
+          })}
+        </Typography>
+        <Typography variant="h6">
+          {this.state.time.toLocaleDateString('en-GB', dateOptions)}
+        </Typography>
+      </div>
+    )
+  }
 }
 
-export default TimeDateDisplay
+export default TimeDateDisplay;
